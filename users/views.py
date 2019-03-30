d6ee2cc895d97bd0d44fa2d796f9eac55515cf29
stones4semper <eloike95@gmail.com>
2019-03-30 02:37:57 +0100

    Add login required for profile

--- users/views.py
+++ users/views.py
@@ -16,7 +16,7 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form, 'title': "Join Now"})
  
-# @login_required
+@login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
